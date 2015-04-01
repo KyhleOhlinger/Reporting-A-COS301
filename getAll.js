537f07c88d15ea15e65969448e7d4b3b837b26e8
sebyg <sebygerber@gmail.com>
2015-04-01 15:30:13 -0700

    Uploading
    I tried to do the uploading but I cannot figure out how to use mongoose,
    maybe someone who has a better understanding than I could try? I have
    managed to get all the pertinent data from the csv though,  its
    basically what Kyhle gave me just fixed up.

--- getAll.js
+++ getAll.js
@@ -2,8 +2,10 @@ var http = require('http');
 var url = require('url');
 var mongoose = require('mongoose');
 var json2csv = require('json2csv');	
+var csv2json = require('csv2json');	
 var fs = require('fs');
 mongoose.connect('mongodb://45.55.154.156:27017/Buzz');
+var Students_Collec;
 http.createServer(
 function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
@@ -13,11 +15,15 @@ function (request, response) {
 	response.write('<input name="btn" id="btn" type="submit" value="View all threads"/><br><br>');
 	
 	response.write('<input name = "btn" id = "btn" type = "submit" value = "View threads for specific buzz"/>');
-        response.write('<input name = "text" id = "text" type = "text" placeholder = "e.g. COS 301"/>');
-	
+        response.write('<input name = "text" id = "text" type = "text" placeholder = "e.g. COS 301"/><br><br>');
 	response.write('</form>');
 	
-
+//-----------------------------------------------------FORM DATA FOR IMPORT FILE-----------------------------------------------------	
+	response.write('<form enctype="multipart/form-data" name="forms" method="get" >');
+	response.write('<input type="file" id ="upload" name="upload" accept=".csv"><br>');
+	response.write('<input name="btn" id="btn" type="submit" value="upload csv"><br><br>');	
+	response.write('</form>');
+	
 	
 	var req = url.parse(request.url, true);
 	if(req.query.btn)
@@ -44,7 +50,7 @@ function (request, response) {
 			);
 
 			
-			var Students_Collec = mongoose.model('Students',StudentSchema); //Defines a model for retrieving Students
+			Students_Collec = mongoose.model('Students',StudentSchema); //Defines a model for retrieving Students
 			var tableHeader =   '<table style="width:100%">'+
 			"<tr>"+
 			"<th>Student Number</th>" +
@@ -74,8 +80,8 @@ function (request, response) {
 				});
 				json = json + '{"Student Number": "","Name": "","Surname": "","PhoneNumber": "","Email": "","Mark": ""}]';//Adding in a blank record to cap it off and prevent trailing commas.
 				json = json.replace("[object Object]","");//Some weird bug that keeps adding this to the beginning of the string,so I just remove it.
-				
-				downloadCSVallStudents(json);
+				exportCSV(json,'Student');
+				//downloadCSVallStudents(json);
 				
 			});
 		}	
@@ -207,7 +213,8 @@ function (request, response) {
 			    });
 				json = json + '{"DateCreated": "","Name": "","PostContent": "","CreatorID": "","SpaceID": "","StatusID": "","Parent": "","Archived": "","AllAttchements": "","PostType": "","Closed": "","ClosingDate": ""}]';//Adding in a blank record to cap it off and prevent trailing commas.
 				json = json.replace("[object Object]","");//Some weird bug that keeps adding this to the beginning of the string,so I just remove it.
-				downloadCSVallThreads(json);
+				//downloadCSVallThreads(json);
+				exportCSV(json,'Threads');
 			    
 			    
 			});
@@ -298,8 +305,142 @@ function (request, response) {
 		    
                 });
             }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------IMPORT FILE TO DATABASE-------------------------------------------------------------------------
 	    
+		if(command == "upload csv")
+		{
+			console.log("uploading file");
+			var selected = req.query.upload;
+
+			var fs = require('fs'),
+			readline = require('readline');
+
+			var rd = readline.createInterface(
+			{
+			    input: fs.createReadStream(selected),
+			    output: process.stdout,
+			    terminal: false
+			});
+			
+			rd.on('line', function(line) 
+			{
+				uploadToDatabase(line);
+			});
+			console.log("File upload complete");
+		}
+	}
+	
+	var data;
+	function uploadToDatabase(a)//Gets all the pertinent data from the csv, but I cannot figure out how to upload it, maybe someone who understands monggose better can fix it?
+	{
+		data=a.split(",");
+		
+		var studentnumber = data[0];
+		var name = data[1];
+		var surname = data[2];
+		var phoneNumber = data[3];
+		var email = data[4];
+		var mark = data[5];
+		
+		/*var stud = new Students_Collec({
+		  std_StudentNumber: studentnumber
+		, std_Name: name
+		, std_Surname: surname  
+		, std_PhoneNumber: phoneNumber
+		, std_Email : email
+		, std_Mark : mark
+		});
+		
+		stud.save(function(err, stud){
+			if(err){
+				throw err;
+				console.log(err);
+			}else{
+				console.log('saved!');
+			}
+		});*/
+		/*var schemaKeyList = ['studentnumber', 'name', 'surname', 'phoneNumber', 'email', 'mark'];
+
+		var studentSchema = new mongoose.Schema({
+			studentnumber: String,
+			name: String,
+			surname: String,
+			phoneNumber: String,
+			email: String,
+			mark: String
+		});
+		var studentUpload = mongoose.model('student', studentSchema);
+
+		function queryAllEntries () {
+			studentUpload.aggregate(
+				{$group: {_id: '$studentnumber', oppArray: {$push: {0
+					name: '$name', 
+					surname: '$surname',
+					phoneNumber: '$phoneNumber',
+					email: '$email',
+					mark: '$mark'
+					}}
+				}}, function(err, qDocList) {
+				console.log(util.inspect(qDocList, false, 10));
+				process.exit(0);
+			});
+		}*/
+		
+		console.log("CURRENT DATA: " + studentnumber + " | " + name + " | " + surname + " | " + phoneNumber + " | " + email + " | " + mark);
+		
+	}
+	
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	function exportCSV(inJson,sub)//Uses json2csv and save the file on the HDD
+	{
+		var json2csv = require('json2csv');
+		var parsedJSON = JSON.parse(inJson);
+		var fs = require('fs');
+
+		var temp ='';
+		for(var obj in parsedJSON)
+		{
+			if(parsedJSON.hasOwnProperty(obj))
+			{
+				for(var prop in parsedJSON[obj])
+				{
+					if(parsedJSON[obj].hasOwnProperty(prop))
+					{
 				
+						if (temp.indexOf(prop) >= 0)
+						{
+						}
+						else
+						{
+							temp+=prop+ "','";
+						}
+					}
+				}
+			}
+		}
+		
+		var newStr = temp.substring(0, temp.length-1);
+		newStr = "'"+newStr;
+		newStr = newStr.replace(/(^\s*,)|(,\s*$)/g, '');
+		var fields=newStr.split(",");
+		for(var i = 0; i < fields.length; i++)
+		{
+			fields[i] = fields[i].replace("'","");
+			fields[i] = fields[i].replace("'","");
+		}
+		console.log("KEYS: " + fields[0]);
+		//var tempJSON = JSON.parse(newStr);
+		json2csv({data: parsedJSON, fields: fields}, function(err, csv) 
+		{
+			if (err) console.log(err);
+			fs.writeFile(sub+'Report.csv', csv, function(err) 
+			{
+				if (err) throw err;
+				console.log('file saved');
+			});
+		});
 	}
 	
 	function downloadCSVallStudents(inJson)//Uses json2csv and save the file on the HDD
